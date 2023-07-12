80357fe0e7d3b0340616b67decd0d5f59ba13f5d
Kiara Johnson <kiarajohnson@kiaras-mbp.lan>
2023-07-11 22:24:40 -0500

    added beginning of function to show questions

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,79 @@
+//Quiz questions
+var questions = [  
+  {
+      title: 'Commonly used data types DO NOT include:',
+      choices: ['strings', 'booleans', 'alerts', 'numbers'],
+      answer: 'alerts',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  }
+  ]
+
+var questionIndex = 0;
+var secondsLeft = 59;
+
+//DOM Elements
+var startEl = document.getElementById("start-btn");
+var timerEl = document.getElementById("timer");
+
+//Click event
+startEl.addEventListener("click", startQuiz);
+
+function startQuiz(){
+  startEl.style.display = "none";
+
+  function showQuestion(){
+    for (let i = 0; i < questions[questionIndex].choices.length; i++){
+      const options = questions[questionIndex].choices[i];
+      var optionsButton = document.createElement("button");
+      optionsButton.textContent = options;
+    }
+    }
+    
+    showQuestion();
+
+    endQuiz();
+
+// console.log(questions[questionIndex].title);
+// questionIndex = questionIndex +1;
+}
+
 // Countdown timer to end quiz
-var timerEl = document.querySelector(".time");
-
-var secondsLeft = 60;
-
 function endQuiz() {
     var timerInterval = setInterval(function(){
         secondsLeft--;
         timerEl.textContent = "Time: " + secondsLeft;
         if(secondsLeft === 0) {
             clearInterval(timerInterval);
-            finalScoreScreen();
+            displayMessage();
         }
     }, 1000);
 }
 
-endQuiz();
+
+// console.log(questions[1].title);
